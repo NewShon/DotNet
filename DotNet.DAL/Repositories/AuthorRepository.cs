70c002bc94555e314f88732afcd32335a668896b
Kiryl Navakshonau <Kiryl_Navakshonau@epam.com>
2019-03-13 18:45:47 +0300

    update genres

--- DotNet.DAL/Repositories/AuthorRepository.cs
+++ DotNet.DAL/Repositories/AuthorRepository.cs
@@ -7,34 +7,46 @@ namespace DotNet.DAL.Repositories
 {
     public class AuthorRepository : IRepository<Author>
     {
-        public void Create()
-        {
-            throw new NotImplementedException();
-        }
+		public void Create(Book book)
+	    {
+		    Warehouse.Books.Add(book);
+	    }
 
-        public void Delete()
-        {
-            throw new NotImplementedException();
-        }
+	    public void Delete(Func<Book, bool> predicate)
+	    {
+		    var booksForDelete = Warehouse.Books.Where(predicate);
+		    foreach (var book in booksForDelete)
+		    {
+			    Warehouse.Books.Remove(book);
 
-        public IEnumerable<Author> Find(Func<Author, bool> predicate)
-        {
-            throw new NotImplementedException();
-        }
+		    }
+	    }
 
-        public Author Get(int id)
-        {
-            throw new NotImplementedException();
-        }
+	    public IEnumerable<Book> Find(Func<Book, bool> predicate)
+	    {
+		    return Warehouse.Books.Where(predicate);
+	    }
 
-        public IEnumerable<Author> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+	    public Book Get(Func<Book, bool> predicate)
+	    {
+		    return Warehouse.Books.Where(predicate).FirstOrDefault();
+	    }
 
-        public void Update()
-        {
-            throw new NotImplementedException();
-        }
-    }
+	    public IEnumerable<Book> GetAll()
+	    {
+		    return Warehouse.Books;
+	    }
+
+	    public void Update(Func<Book, bool> predicate, Book book)
+	    {
+		    var bookForUpdate = Warehouse.Books.Where(predicate).FirstOrDefault();
+		    if (bookForUpdate != null)
+		    {
+			    bookForUpdate.Name = book.Name;
+			    bookForUpdate.Authors = book.Authors;
+			    bookForUpdate.Description = book.Description;
+			    bookForUpdate.Genres = book.Genres;
+		    }
+	    }
+	}
 }
--- DotNet.DAL/Repositories/GenreRepository.cs
+++ DotNet.DAL/Repositories/GenreRepository.cs
@@ -1,40 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using DotNet.DAL.Entities;
 using DotNet.DAL.Interfaces;
 
 namespace DotNet.DAL.Repositories
 {
-    public class GenreRepository : IRepository<Genre>
-    {
-        public void Create()
-        {
-            throw new NotImplementedException();
-        }
+	public class GenreRepository : IRepository<Genre>
+	{
+		public void Create(Genre genre)
+		{
+			Warehouse.Genres.Add(genre);
+		}
 
-        public void Delete()
-        {
-            throw new NotImplementedException();
-        }
+		public void Delete(Func<Genre, bool> predicate)
+		{
+			var booksForDelete = Warehouse.Genres.Where(predicate);
+			foreach (var genre in booksForDelete)
+			{
+				Warehouse.Genres.Remove(genre);
+			}
+		}
 
-        public IEnumerable<Genre> Find(Func<Genre, bool> predicate)
-        {
-            throw new NotImplementedException();
-        }
+		public IEnumerable<Genre> Find(Func<Genre, bool> predicate)
+		{
+			return Warehouse.Genres.Where(predicate);
+		}
 
-        public Genre Get(int id)
-        {
-            throw new NotImplementedException();
-        }
+		public Genre Get(Func<Genre, bool> predicate)
+		{
+			return Warehouse.Genres.Where(predicate).FirstOrDefault();
+		}
 
-        public IEnumerable<Genre> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+		public IEnumerable<Genre> GetAll()
+		{
+			return Warehouse.Genres;
+		}
 
-        public void Update()
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public void Update(Func<Genre, bool> predicate, Genre genre)
+		{
+			var genreForUpdate = Warehouse.Genres.Where(predicate).FirstOrDefault();
+			if (genreForUpdate != null)
+			{
+				genreForUpdate.Name = genre.Name;
+				genreForUpdate.Books = genre.Books;
+			}
+		}
+	}
 }
