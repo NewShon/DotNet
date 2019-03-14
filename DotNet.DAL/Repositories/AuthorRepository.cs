bf8af96dbe71db7eb749e07f4f1851d485d4768a
Kiryl Navakshonau <Kiryl_Navakshonau@epam.com>
2019-03-14 14:29:49 +0300

    author repo add logic

--- DotNet.DAL/Repositories/AuthorRepository.cs
+++ DotNet.DAL/Repositories/AuthorRepository.cs
@@ -1,52 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using DotNet.DAL.Entities;
 using DotNet.DAL.Interfaces;
 
 namespace DotNet.DAL.Repositories
 {
-    public class AuthorRepository : IRepository<Author>
-    {
-		public void Create(Book book)
-	    {
-		    Warehouse.Books.Add(book);
-	    }
+	public class AuthorRepository : IRepository<Author>
+	{
+		public void Create(Author author)
+		{
+			Warehouse.Authors.Add(author);
+		}
 
-	    public void Delete(Func<Book, bool> predicate)
-	    {
-		    var booksForDelete = Warehouse.Books.Where(predicate);
-		    foreach (var book in booksForDelete)
-		    {
-			    Warehouse.Books.Remove(book);
+		public void Delete(Func<Author, bool> predicate)
+		{
+			var authorsForDelete = Warehouse.Authors.Where(predicate);
+			foreach (var author in authorsForDelete)
+			{
+				Warehouse.Authors.Remove(author);
+			}
+		}
 
-		    }
-	    }
+		public IEnumerable<Author> Find(Func<Author, bool> predicate)
+		{
+			return Warehouse.Authors.Where(predicate);
+		}
 
-	    public IEnumerable<Book> Find(Func<Book, bool> predicate)
-	    {
-		    return Warehouse.Books.Where(predicate);
-	    }
+		public Author Get(Func<Author, bool> predicate)
+		{
+			return Warehouse.Authors.Where(predicate).FirstOrDefault();
+		}
 
-	    public Book Get(Func<Book, bool> predicate)
-	    {
-		    return Warehouse.Books.Where(predicate).FirstOrDefault();
-	    }
+		public IEnumerable<Author> GetAll()
+		{
+			return Warehouse.Authors;
+		}
 
-	    public IEnumerable<Book> GetAll()
-	    {
-		    return Warehouse.Books;
-	    }
-
-	    public void Update(Func<Book, bool> predicate, Book book)
-	    {
-		    var bookForUpdate = Warehouse.Books.Where(predicate).FirstOrDefault();
-		    if (bookForUpdate != null)
-		    {
-			    bookForUpdate.Name = book.Name;
-			    bookForUpdate.Authors = book.Authors;
-			    bookForUpdate.Description = book.Description;
-			    bookForUpdate.Genres = book.Genres;
-		    }
-	    }
+		public void Update(Func<Author, bool> predicate, Author author)
+		{
+			var authorsForUpdate = Warehouse.Authors.Where(predicate).FirstOrDefault();
+			if (authorsForUpdate != null)
+			{
+				authorsForUpdate.Name = author.Name;
+				authorsForUpdate.Books = author.Books;
+			}
+		}
 	}
 }
