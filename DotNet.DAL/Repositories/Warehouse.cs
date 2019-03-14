bd9999a440fc98cd77910241f68f8ca8a2c515d6
NewShon <kirill31_95@mail.ru>
2019-03-15 01:35:53 +0300

    fix: static null exception

--- DotNet.DAL/Repositories/Warehouse.cs
+++ DotNet.DAL/Repositories/Warehouse.cs
@@ -17,21 +17,21 @@ namespace DotNet.DAL.Repositories
             Books = new List<Book>
             {
                 new Book { Id = 1, Name = "Anarchy of decay", Description = "Good book 1" },
-                new Book { Id = 2, Name = "Illusion of freedom", Description = "Good book 2"  },
-                new Book { Id = 3, Name = "Legendary moonlight sculptor", Description = "Good book 3"  }
+                new Book { Id = 2, Name = "Illusion of freedom", Description = "Good book 2" },
+                new Book { Id = 3, Name = "Legendary moonlight sculptor", Description = "Good book 3" }
             };
 
             Genres = new List<Genre>
             {
-                new Genre { Id = 1, Name = "Apocalypse", Books = { Books[0] } },
-                new Genre { Id = 2, Name = "Philosophy", Books = { Books[1] } },
-                new Genre { Id = 3, Name = "RPG", Books = { Books[2] } }
+                new Genre { Id = 1, Name = "Apocalypse" },
+                new Genre { Id = 2, Name = "Philosophy" },
+                new Genre { Id = 3, Name = "RPG" }
             };
 
             Authors = new List<Author>
             {
-                new Author { Id = 1, Name = "Michael", Books = { Books[0], Books[2] } },
-                new Author { Id = 2, Name = "Raichel", Books = { Books[1] } }
+                new Author { Id = 1, Name = "Michael" },
+                new Author { Id = 2, Name = "Raichel" }
             };
         }
     }
